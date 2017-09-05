1f8a269aed57edcef718e0a07e850684df914cab
fabian camargo <fabian camargo>
2017-09-04 20:24:18 -0500

    Add project files.

---
+++ EmotionPlatzi/Models/EmoPicture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmotionPlatzi.Models
+{
+    public class EmoPicture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        public virtual ObservableCollection<EmoFace> Faces { get; set; }
+    }
+}
